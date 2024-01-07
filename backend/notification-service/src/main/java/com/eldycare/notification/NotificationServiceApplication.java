d837edd25ee8f9041dd687010906cfc7178f6343
Schrodingdong <91998001+Schrodingdong@users.noreply.github.com>
2024-01-07 13:35:30 +0100

    [FRONT] Refatoring code a bit

--- backend/notification-service/src/main/java/com/eldycare/notification/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/eldycare/notification/NotificationServiceApplication.java
@@ -4,11 +4,6 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableRabbit
 @SpringBootApplication
